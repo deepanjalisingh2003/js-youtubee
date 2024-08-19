2ca5b7aa65a64d8364043c5b11a688f003130576
deepanjalisingh2003 <122670108+deepanjalisingh2003@users.noreply.github.com>
2024-08-19 06:54:33 +0000

    variable

---
+++ 01/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js as newer version
+
+let name="deepa"
+let age=20
+let islooged=true
+
+/*
+String 
+number ==> 2 to power 53
+bigint
+null ==> standlone value
+undefined
+boolean ==> true/false
+symbol ==> unique
+*/
+
+console.log(typeof null)  //object
+console.log(typeof undefined)   //undefined
---
+++ 01/variable.js
@@ -0,0 +1,10 @@
+const account_id=4563
+let account_email="deepa@gmail.com"
+var account_pass="1234"
+let account_state;
+
+//account_id=2   //not allowed constant change
+account_email="dee@06gmail.com"
+account_pass="2433"
+console.log(account_id);
+console.table([account_email,account_id,account_pass,account_state])
