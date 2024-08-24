287b204ae6b2a0c9122e8ec2c16dc224804a1b79
deepanjalisingh2003 <122670108+deepanjalisingh2003@users.noreply.github.com>
2024-08-24 15:15:50 +0000

    conversion of datatypes

--- 01/conversion.js
+++ 01/conversion.js
@@ -1,1 +1,23 @@
+let score="33"
 
+console.log(typeof score);
+let value=Number(score)
+console.log(typeof value);
+
+
+//"33" == 33
+//"33abc"==NaN(not a number)
+//true ==1,false==0
+
+let islogged=1
+let booleanIsLogged=Boolean(islogged);
+console.log(booleanIsLogged);
+
+// ""=false
+// 1="true"
+//0="false"
+//"deepa"=true
+
+let somenum=33
+let stringnum=String(somenum);
+console.log( typeof stringnum);
