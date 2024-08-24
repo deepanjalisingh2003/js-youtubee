07eea2b5fb4d98fd341d57a4e7112fa2be9568cf
deepanjalisingh2003 <122670108+deepanjalisingh2003@users.noreply.github.com>
2024-08-24 15:29:53 +0000

    conversion02

---
+++ 01/conversion02.js
@@ -0,0 +1,29 @@
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+let str1="hello"
+let str2=" deepa"
+let str3=str1+str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log(true);  //true
+console.log(+true);  //1
+//console.log(true+); //error
+console.log(+"");  //0
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++; //101
+++gamecounter; //101
+console.log(gamecounter);
