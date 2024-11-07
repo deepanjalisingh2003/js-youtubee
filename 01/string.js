e10271805bd4429cb8b87505d5ae5a8540c7f76f
deepanjalisingh2003 <122670108+deepanjalisingh2003@users.noreply.github.com>
2024-11-07 23:32:18 +0530

    Update string.js

--- 01/string.js
+++ 01/string.js
@@ -1,1 +1,28 @@
+const name="deepa"
+const repo=50
+
+console.log (name+repo+"value");
+console.log (`hello my name is ${name} and my repo count is ${repo}`);
+
+const gameName=new String('deepa')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log (gameName.length);
+console.log (gameName.toUpperCase());
+console.log (gameName.charAt(2));
+console.log (gameName.indexOf('t'));
+
+const new = gameName.substring(0,4); //negative value will be ignored
+console.log (new);
+ const another= gameName.slice(-8,4)    //start from reverse 
+console.log (another);
+
+//trim ---- extra space remove 
+//replace - search and replace the value 
+//includes --- variable include or not
+
+console.log (gameName.split('-'));
+
+
 
